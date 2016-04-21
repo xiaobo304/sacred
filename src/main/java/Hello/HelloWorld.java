cdbf7e2147a9d7d436f2b6808be739e702bc2afb
xiaobo.sha <xiaobo304@hotmail.com>
2016-04-21 23:19:55 +0800

    hello

---
+++ src/main/java/Hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package Hello;
+
+/**
+ * Created by xiaobo on 16/4/21.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.printf("hello world");
+    }
+}
